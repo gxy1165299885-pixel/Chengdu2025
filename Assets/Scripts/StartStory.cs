f7e6782b6d76239b1abe96203ad023b48d60a3b2
熬夜拧螺丝 <shengzechen23@mails.ucas.ac.cn>
2025-12-21 11:02:31 +0800

    .

---
+++ Assets/Scripts/StartStory.cs
@@ -0,0 +1,49 @@
+﻿using Architecture;
+using UnityEngine;
+using UnityEngine.UI;
+using Yarn.Unity;
+
+namespace DefaultNamespace
+{
+    public class StartStory : SingletonMono<StartStory>
+    {
+        [SerializeField] private Image characterImage;
+        [SerializeField] private Sprite[] characterSprites;
+        
+        [YarnCommand("CharacterHappy")]
+        public static void CharacterHappy()
+        {
+            Instance.characterImage.sprite = Instance.characterSprites[0];
+        }
+        
+        [YarnCommand("CharacterAmaze")]
+        public static void CharacterAmaze()
+        {
+            Instance.characterImage.sprite = Instance.characterSprites[1];
+        }
+        
+        [YarnCommand("CharacterNormal")]
+        public static void CharacterNormal()
+        {
+            Instance.characterImage.sprite = Instance.characterSprites[2];
+        }
+        
+        [YarnCommand("CharacterSigh")]
+        public static void CharacterSigh()
+        {
+            Instance.characterImage.sprite = Instance.characterSprites[3];
+        }
+        
+        [YarnCommand("CharacterDigust1")]
+        public static void CharacterDigust1()
+        {
+            Instance.characterImage.sprite = Instance.characterSprites[4];
+        }
+        
+        [YarnCommand("CharacterDigust2")]
+        public static void CharacterDigust2()
+        {
+            Instance.characterImage.sprite = Instance.characterSprites[5];
+        }
+    }
+}
